8d222bcca400c9804b09907449c49fd9cee907e7
Danilo Augusto <dandevweb@gmail.com>
2023-10-13 12:54:07 -0300

    test: it should be able to create a product

--- tests/Feature/ActionsTest.php
+++ tests/Feature/ActionsTest.php
@@ -1,11 +1,14 @@
 <?php
 
 use App\Models\User;
+use App\Models\Product;
 use function Pest\Laravel\actingAs;
-
 use function Pest\Laravel\postJson;
 use App\Actions\CreateProductAction;
+use function Pest\Laravel\assertDatabaseHas;
 use Illuminate\Support\Facades\Notification;
+use App\Notifications\NewProductNotification;
+use function Pest\Laravel\assertDatabaseCount;
 
 it('should call the action to create a product', function () {
     Notification::fake();
@@ -23,4 +26,21 @@ it('should call the action to create a product', function () {
     postJson(route('products.store'), [
         'title' => $title,
     ]);
+});
+
+
+it('should be able to create a product', function () {
+    Notification::fake();
+
+    $user = User::factory()->create();
+
+    (new CreateProductAction())->handle('Product 1', $user);
+
+    assertDatabaseCount(Product::class, 1);
+    assertDatabaseHas(Product::class, [
+        'title' => 'Product 1',
+        'owner_id' => $user->id,
+    ]);
+
+    Notification::assertSentTo($user, NewProductNotification::class);
 });
